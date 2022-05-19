ea997f220dc52a41d9a5cc20fdf85cf7c4761b3c
shivjo <tyler.shivers@verizon.com>
2022-05-19 09:09:16 -0400

    feat: add game.js. add game loop
    loops functions based on seconds since last loop and renders based on
    how many times the snake should move per second

--- game.js
+++ game.js
@@ -1,11 +1,19 @@
 
-
+let lastRenderTime = 0;
+const SNAKE_SPEED = 2; // 2 times per second
 
 // This is the game loop that requests a render
 // from the browser then executes the other functions
 function main(currentTime){
     window.requestAnimationFrame(main) // request the frame and wait then calls the main func
-    console.log(currentTime)
+    const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
+    if (secondsSinceLastRender < 1 / SNAKE_SPEED) return;
+
+    lastRenderTime = currentTime;
+    console.log("render")
+
+    // update();
+    // draw();
 }
 
 window.requestAnimationFrame(main) // request the first time
