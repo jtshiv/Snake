57a14c883f3b993cdf05f763a7372269284835c1
shivjo <tyler.shivers@verizon.com>
2022-05-20 08:59:13 -0400

    feat: add grid to board. fix spelling.

--- game.js
+++ game.js
@@ -1,16 +1,17 @@
 import { getSnakeHead, snakeIntersection, update as updateSnake, draw as drawSnake, SNAKE_SPEED } from './snake.js';
 import {update as updateFood, draw as drawFood} from './food.js';
-import {outsideGrid} from './grid.js';
+import {outsideGrid,draw as drawGrid} from './grid.js';
 
 let lastRenderTime = 0;
 let gameOver = false;
+let gridMade=false;
 const gameBoard = document.getElementById("game-board");
 
 // This is the game loop that requests a render
 // from the browser then executes the other functions
 function main(currentTime){
     if (gameOver){
-        if (confirm('You loss. Press ok to restart.')){
+        if (confirm('You lose. Press ok to restart.')){
             window.location = '/';
         }
         return;
@@ -34,10 +35,25 @@ function update(){
     checkDeath();
 }
 
+function undrawItems(gameBoard){
+    Array.from(gameBoard.getElementsByClassName('snake')).forEach(x=>{
+        x.remove();
+    })
+    Array.from(gameBoard.getElementsByClassName('food')).forEach(x=>{
+        x.remove();
+    })
+}
+
 function draw() {
-    gameBoard.innerHTML = ""; // clear old snake bodies
+    // gameBoard.innerHTML = ""; // clear old snake bodies
+    undrawItems(gameBoard);
     drawSnake(gameBoard);
     drawFood(gameBoard);
+    if(!gridMade) {
+        gridMade = true;
+        drawGrid(gameBoard);
+    }  
+    
 }
 
 function checkDeath(){
--- grid.js
+++ grid.js
@@ -13,3 +13,16 @@ export function outsideGrid(position){
         position.y < 1 || position.y > GRID_SIZE
     )
 }
+
+export function draw(gameBoard){
+    for (let x=0;x<=GRID_SIZE;x++){
+        for (let y=0;y<=GRID_SIZE;y++){
+            const gridElement = document.createElement('div');
+            gridElement.style.gridRowStart = y;
+            gridElement.style.gridColumnStart = x;
+            gridElement.classList.add('grid');
+            gameBoard.appendChild(gridElement)
+
+        }
+    }
+}
