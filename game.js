a49d2f4759ba3d3bb0550a027ee2099ae4a3404f
tyler <shiversjt@gmail.com>
2022-05-21 04:18:43 +0100

    fix: on death page resets to current
    it was just / before which worked right up until I had it hosted on the
    pi. it now refreshes the exact page

--- game.js
+++ game.js
@@ -12,7 +12,7 @@ const gameBoard = document.getElementById("game-board");
 function main(currentTime){
     if (gameOver){
         if (confirm('You lose. Press ok to restart.')){
-            window.location = '/';
+            window.location = window.location.href;
         }
         return;
     }
