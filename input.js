623b95863037251d05de238e5dd8932305886db6
Tyler Shivers <shiversjt@gmail.com>
2022-05-20 22:52:28 -0400

    feat: add touch input to snake

--- input.js
+++ input.js
@@ -1,5 +1,7 @@
 let inputDirection = {x:0,y:0};
 let lastInputDirection = {x:0,y:0};
+let touchStart = {x:null,y:null};
+let touchEnd = {x:null,y:null};
 
 
 window.addEventListener('keydown',e => {
@@ -22,6 +24,37 @@ window.addEventListener('keydown',e => {
             break;
     }
 })
+
+function handleTouch(){
+	let touchDiff = {x:touchEnd.x-touchStart.x,y:touchEnd.y-touchStart.y};
+	//let ratio = touchDiff.x / touchDiff.y;
+	console.log(Math.abs(touchDiff.x)>Math.abs(touchDiff.y));
+	if (Math.abs(touchDiff.x)>Math.abs(touchDiff.y)){ // change in x
+		if (touchDiff.x>0){
+		    if (lastInputDirection.x===0) inputDirection = {x:1,y:0};
+		} else {
+		    if (lastInputDirection.x===0) inputDirection = {x:-1,y:0};
+		}
+	} else {
+		if (touchDiff.y>0){
+		    if (lastInputDirection.y===0) inputDirection = {x:0,y:1};
+		} else {
+		    if (lastInputDirection.y===0) inputDirection = {x:0,y:-1};;
+		}
+
+	}
+}
+
+window.addEventListener('touchstart', e => {
+	touchStart.x = e.changedTouches[0].screenX;
+	touchStart.y = e.changedTouches[0].screenY;
+});
+window.addEventListener('touchend', e => {
+	touchEnd.x = e.changedTouches[0].screenX;
+	touchEnd.y = e.changedTouches[0].screenY;
+	handleTouch();
+})
+	
 
 export function getInputDirection(){
     lastInputDirection = inputDirection;
